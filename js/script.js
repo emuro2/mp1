8ad39455041b91221d0891d46f1bd53414648eaa
emuro2 <erikmuro@gmail.com>
2015-02-10 22:07:55 -0600

    updated git repo with current files

--- js/script.js
+++ js/script.js
@@ -1,16 +1,41 @@
-var divs = document.getElementsByTagName('div');
-for(var i=0; i<divs.length; i++) {
-  divs[i].addEventListener("click", highlightThis);
-  /*
-  divs[i].addEventListener("click", highlightThis, true);
-  divs[i].addEventListener("click", highlightThis, false);*/
+// function(event) {
+// 		event.preventDefault();
+// 	    $('html, body').animate({
+// 	        scrollTop: $(event.data.target).offset().top
+// 	    }, 1000);
+// 	}
+
+
+var blue ="#2176C7";
+var black = "#212121";
+
+
+var litems = document.getElementsByTagName('a');
+for(var i=0; i<litems.length; i++) {
+  	//litems[i].addEventListener("click", scroll);
+	litems[i].addEventListener("mouseover", mouseOver);
+	litems[i].addEventListener("mouseout", mouseOut);
 }
 
-function highlightThis(event) {
-    //event.stopPropagation();
-  
-    var backgroundColor = this.style.backgroundColor;
-    this.style.backgroundColor='yellow';
-    alert(this.className);
-    this.style.backgroundColor=backgroundColor;
+
+$(document).ready(function() { 
+	$("#home-button").click({target:"#home-section", padding:0},scroll);
+	$("#portfolio-button").click({target:"#portfolio-section", padding:100},scroll);
+	$("#about-button").click({target:"#about-section", padding:0},scroll);
+	$("#contact-button").click({target:"#contact-section", padding:0},scroll);
+});
+
+function scroll(event) {
+	event.preventDefault();
+	$('html, body').animate({
+ 	    scrollTop: $(event.data.target).position().top - event.data.padding
+ 	}, 1000);
+}
+
+function mouseOver(event) {
+	this.style.color=blue;
+}
+
+function mouseOut(event) {
+	this.style.color=black;
 }
