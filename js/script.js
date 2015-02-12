83d97a74a4e61e83b309cbb0ed65b8a13f513dce
emuro2 <erikmuro@gmail.com>
2015-02-11 21:46:35 -0600

    better layout, static image, carousel positioning

--- js/script.js
+++ js/script.js
@@ -7,32 +7,35 @@ var black = "#212121";
 
 var home_button = $("#erik-home-button");
 var menu_button = $(".menu-button");
+var arrow_buttons = $(".arrows");
 
-$(document).ready(function() { 
+$(document).ready(function() {
+    //home button ("Erik Muro")
 	home_button.click(scroll);
     home_button.mouseover(mouseOver);
     home_button.mouseout(mouseOut);
 
-
+    //menu buttons (i.e. About)
     menu_button.click(scroll);
     menu_button.mouseover(mouseOver);
     menu_button.mouseout(mouseOut);
 
-    //
-    //$("#right-arrow").mouseover(
-    //        function(){
-    //            event.preventDefault();
-    //            this.className= "fa fa-arrow-circle-o-right fa-5x"
-    //        }
-    //    );
-    //$("#right-arrow").mouseout(
-    //    function(){
-    //        event.preventDefault();
-    //        this.className= "fa fa-arrow-circle-o-right fa-4x"
-    //    }
-    //);
+    //carousel arrows
+    arrow_buttons.mouseover(mouseOver);
+    arrow_buttons.mouseout(mouseOut);
+
+
+    //// Set the interval to be 5 seconds
+    //var t = setInterval(function(){
+    //    $("#carousel").animate({marginLeft:-1200},1000,function(){
+    //        $(this).find("li:last").after($(this).find("li:first"));
+    //        $(this).css({marginLeft:0});
+    //    })
+    //},5000);
 });
 
+
+//smooth scrolling
 function scroll(event) {
 	event.preventDefault();
     target = this.hash;
@@ -45,10 +48,36 @@ function scroll(event) {
     }, 1000);
 }
 
+
 function mouseOver(event) {
-	this.style.color=blue;
+    target = this.hash;
+    if(target === "#right" || target === "#left") {
+        if (target === "#right") {
+            var arrow = document.getElementById("right");
+            arrow.className = "fa fa-arrow-circle-o-right fa-5x";
+        }
+        else {
+            var arrow = document.getElementById("left");
+            arrow.className = "fa fa-arrow-circle-o-left fa-5x";
+        }
+    }
+    else {
+        this.style.color = blue;
+    }
 }
 
 function mouseOut(event) {
-	this.style.color=black;
+    if(this.hash === "#right" || this.hash === "#left") {
+        if (target === "#right") {
+            var arrow = document.getElementById("right");
+            arrow.className = "fa fa-arrow-circle-o-right fa-4x";
+        }
+        else {
+            var arrow = document.getElementById("left");
+            arrow.className = "fa fa-arrow-circle-o-left fa-4x";
+        }
+    }
+    else {
+        this.style.color = black;
+    }
 }
